01b29ce0dc10e86a71c79b2d8ad32fa5eb769227
Maksim Pecherskiy <max@maksimize.com>
2014-10-17 23:46:30 -0700

    Switch to express serving.'

--- server.js
+++ server.js
@@ -1,38 +1,137 @@
-var Hapi = require('hapi');
+/* global console */
+var path = require('path');
+var express = require('express');
+var helmet = require('helmet');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var Moonboots = require('moonboots-express');
+var moonBootsConfig = require('./moonbootsConfig');
+var compress = require('compression');
 var config = require('getconfig');
-var server = new Hapi.Server(config.http.listen, config.http.port);
-var moonbootsConfig = require('./moonbootsConfig');
-var fakeApi = require('./fakeApi');
-var internals = {};
+var semiStatic = require('semi-static');
+var serveStatic = require('serve-static');
+var stylizer = require('stylizer');
+var templatizer = require('templatizer');
+var app = express();
 
-// set clientconfig cookie
-internals.configStateConfig = {
-    encoding: 'none',
-    ttl: 1000 * 60 * 15,
-    isSecure: config.isSecure
+// a little helper for fixing paths for various environments
+var fixPath = function (pathString) {
+    return path.resolve(path.normalize(pathString));
 };
-server.state('config', internals.configStateConfig);
-internals.clientConfig = JSON.stringify(config.client);
-server.ext('onPreResponse', function(request, reply) {
-    if (!request.state.config) {
-        var response = request.response;
-        return reply(response.state('config', encodeURIComponent(internals.clientConfig)));
-    }
-    else {
-        return reply();
-    }
+
+
+// -----------------
+// Configure express
+// -----------------
+app.use(compress());
+app.use(serveStatic(fixPath('public')));
+
+// we only want to expose tests in dev
+if (config.isDev) {
+    app.use(serveStatic(fixPath('test/assets')));
+    app.use(serveStatic(fixPath('test/spacemonkey')));
+}
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// in order to test this with spacemonkey we need frames
+if (!config.isDev) {
+    app.use(helmet.xframe());
+}
+app.use(helmet.xssFilter());
+app.use(helmet.nosniff());
+
+app.set('view engine', 'jade');
+
+
+// -----------------
+// Set up our little demo API
+// -----------------
+var api = require('./fakeApi');
+app.get('/api/people', api.list);
+app.get('/api/people/:id', api.get);
+app.delete('/api/people/:id', api.delete);
+app.put('/api/people/:id', api.update);
+app.post('/api/people', api.add);
+
+
+// -----------------
+// Enable the functional test site in development
+// -----------------
+if (config.isDev) {
+    app.get('/test*', semiStatic({
+        folderPath: fixPath('test'),
+        root: '/test'
+    }));
+}
+
+
+// -----------------
+// Set our client config cookie
+// -----------------
+app.use(function (req, res, next) {
+    res.cookie('config', JSON.stringify(config.client));
+    next();
 });
 
 
-// require moonboots_hapi plugin
-server.pack.register({plugin: require('moonboots_hapi'), options: moonbootsConfig}, function (err) {
-    if (err) throw err;
-    server.pack.register(fakeApi, function (err) {
-        if (err) throw err;
-        // If everything loaded correctly, start the server:
-        server.start(function (err) {
-            if (err) throw err;
-            console.log("PP Dash # is running at: http://localhost:" + config.http.port + " Yep. That\'s pretty awesome.");
-        });
-    });
+// ---------------------------------------------------
+// Configure Moonboots to serve our client application
+// ---------------------------------------------------
+
+// for reuse
+var appDir = __dirname + '/client';
+var cssDir = __dirname + '/public/css';
+var libDir = __dirname + '/public/jslib';
+
+
+new Moonboots({
+    moonboots: {
+        jsFileName: 'ppdash',
+        cssFileName: 'ppdash',
+        main: fixPath(appDir + '/app.js'),
+        developmentMode: config.isDev,
+        libraries: [
+          libDir + '/hello.all.min.js'
+        ],
+        stylesheets: [
+          fixPath(cssDir + '/bootstrap.css'),
+          fixPath(cssDir + '/cosmo.css'),
+          fixPath(cssDir + '/keen.dashboard.css'),
+          fixPath(cssDir + '/app.css')
+        ],
+        browserify: {
+            debug: false
+        },
+        beforeBuildJS: function () {
+            // This re-builds our template files from jade each time the app's main
+            // js file is requested. Which means you can seamlessly change jade and
+            // refresh in your browser to get new templates.
+            if (config.isDev) {
+                templatizer(fixPath('templates'), fixPath(appDir + '/templates.js'));
+            }
+        },
+        beforeBuildCSS: function (done) {
+            // This re-builds css from stylus each time the app's main
+            // css file is requested. Which means you can seamlessly change stylus files
+            // and see new styles on refresh.
+            if (config.isDev) {
+                stylizer({
+                    infile: fixPath(cssDir + '/app.styl'),
+                    outfile: fixPath(cssDir + '/app.css'),
+                    development: true
+                }, done);
+            } else {
+                done();
+            }
+        }
+    },
+    server: app
 });
+
+
+// listen for incoming http requests on the port as specified in our config
+app.listen(config.http.port);
+console.log("afd is running at: http://localhost:" + config.http.port + " Yep. That\'s pretty awesome.");
